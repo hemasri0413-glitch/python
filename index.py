5bb3327784f9f1bb73c472a0d166db2fb920d691
hemasri0413-glitch <hemasri0413@gmail.com>
2026-09-03 12:55:13 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+#age eligibility checker
+age = int(input("Enter your age"))
+
+print("Eligible:", age >= 18)
+
+#pass or fail
+marks = int(input("Enter your marks:"))
+
+print("Password:", marks >= 40)
